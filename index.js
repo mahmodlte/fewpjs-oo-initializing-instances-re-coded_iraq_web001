bde72368bf2a9366bd090b732ae7fe709259329f
Mahmoud Mansour <mahmodlte@gmail.com>
2020-06-13 07:29:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 // Write your code here
 class Breakfast {
-  constractior 
+  constructor(food, drink){
+    this.food = food;
+    this.drink = drink;
+  } 
+}
+class Lunch {
+  constructor (salad,soup,drink){
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
 }
